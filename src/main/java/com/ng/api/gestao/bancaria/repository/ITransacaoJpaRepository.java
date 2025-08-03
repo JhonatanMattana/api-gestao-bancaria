c88ab7126e98ccbb00a75cdb147962722fba63cf
JhonatanMattana <kaverinhaa@hotmail.com>
2025-08-03 14:04:09 -0300

    feat - criado ITransacaoJpaRepository

--- src/main/java/com/ng/api/gestao/bancaria/repository/ITransacaoJpaRepository.java
+++ src/main/java/com/ng/api/gestao/bancaria/repository/ITransacaoJpaRepository.java
@@ -1,11 +1,11 @@
 package com.ng.api.gestao.bancaria.repository;
 
-import com.ng.api.gestao.bancaria.model.Conta;
+import com.ng.api.gestao.bancaria.model.Transacao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
-public interface IContaJpaRepository extends JpaRepository<Conta, UUID> {
+public interface ITransacaoJpaRepository extends JpaRepository<Transacao, UUID> {
 }
