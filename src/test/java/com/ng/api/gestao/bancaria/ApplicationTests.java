447cf61eda1681bd784dcda3c61fd1287b7c5161
JhonatanMattana <kaverinhaa@hotmail.com>
2025-08-03 13:39:50 -0300

    criado projeto com spring boot

---
+++ src/test/java/com/ng/api/gestao/bancaria/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ng.api.gestao.bancaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
