a897b3b2193312a28ff3ce799e6bd4c2c4099c5e
JhonatanMattana <kaverinhaa@hotmail.com>
2025-08-03 13:59:34 -0300

    feat - criado dto ContaDto

---
+++ src/main/java/com/ng/api/gestao/bancaria/dto/ContaDto.java
@@ -0,0 +1,16 @@
+package com.ng.api.gestao.bancaria.dto;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode( of = "numero_conta")
+public class ContaDto {
+    @NotNull(message = "Obrigatório informar o número da Conta")
+    Integer numero_conta;
+    @NotNull(message = "Obrigatório informar o Saldo da Conta")
+    float saldo;
+}
