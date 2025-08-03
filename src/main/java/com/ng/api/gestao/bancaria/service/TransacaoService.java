303cf8d718ba24e27425853df8b787a355f1b2a7
JhonatanMattana <kaverinhaa@hotmail.com>
2025-08-03 14:06:43 -0300

    feat - criado TransacaoService

---
+++ src/main/java/com/ng/api/gestao/bancaria/service/TransacaoService.java
@@ -0,0 +1,9 @@
+package com.ng.api.gestao.bancaria.service;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class TransacaoService {
+}
