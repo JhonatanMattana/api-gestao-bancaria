3103cf4acca8e28e3f3580915c4a0f7ecf2f6b8e
JhonatanMattana <kaverinhaa@hotmail.com>
2025-08-03 14:41:40 -0300

    feat - criado class TransacaoController

---
+++ src/main/java/com/ng/api/gestao/bancaria/controller/TransacaoController.java
@@ -0,0 +1,9 @@
+package com.ng.api.gestao.bancaria.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/transacao")
+public class TransacaoController {
+}
