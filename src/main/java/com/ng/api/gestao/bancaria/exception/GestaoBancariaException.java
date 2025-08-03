587b83f3ed7ee044161ec4098c0aa0f21eaee60f
JhonatanMattana <kaverinhaa@hotmail.com>
2025-08-03 14:25:53 -0300

    feat - implemetado GestaoBancariaException

--- src/main/java/com/ng/api/gestao/bancaria/exception/GestaoBancariaException.java
+++ src/main/java/com/ng/api/gestao/bancaria/exception/GestaoBancariaException.java
@@ -1,4 +1,17 @@
 package com.ng.api.gestao.bancaria.exception;
 
-public class GestaoBancariaException {
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+public class GestaoBancariaException extends RuntimeException {
+
+    private static final long serialVersionUID = -919613333877425950L;
+
+    @Getter
+    private HttpStatus httpStatus;
+
+    public GestaoBancariaException(String mensagem) {
+        super(mensagem);
+        this.httpStatus = HttpStatus.NOT_FOUND;
+    }
 }
