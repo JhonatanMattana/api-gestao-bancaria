3ab2423400b993bf002cc7c4495125940c8dbd98
JhonatanMattana <kaverinhaa@hotmail.com>
2025-08-03 14:35:23 -0300

    feat - adicionado validações ao criarConta  e ao consultar Conta

--- src/main/java/com/ng/api/gestao/bancaria/service/ContaService.java
+++ src/main/java/com/ng/api/gestao/bancaria/service/ContaService.java
@@ -3,6 +3,7 @@ package com.ng.api.gestao.bancaria.service;
 import com.ng.api.gestao.bancaria.controller.dto.ContaDto;
 import com.ng.api.gestao.bancaria.model.Conta;
 import com.ng.api.gestao.bancaria.repository.ContaRepository;
+import com.ng.api.gestao.bancaria.validator.ContaServiceValidator;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -10,13 +11,16 @@ import org.springframework.stereotype.Service;
 @AllArgsConstructor
 public class ContaService {
     private final ContaRepository repository;
+    private final ContaServiceValidator validator;
 
     public ContaDto criarConta(ContaDto dto) {
+        validator.validarCriarConta(dto);
         Conta conta = repository.salvar(dto);
         return new ContaDto(conta.getNumero(), conta.getSaldo());
     }
 
     public ContaDto getContaPorNumero(Integer numeroConta) {
+        validator.validarExistConta(numeroConta);
         Conta conta = repository.getContaByNumero(numeroConta);
         return new ContaDto(conta.getNumero(), conta.getSaldo());
     }
