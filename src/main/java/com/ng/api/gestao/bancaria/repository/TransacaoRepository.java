f5aba64f54ba0bfaaa5c3418eda4b92df9a37f6f
JhonatanMattana <kaverinhaa@hotmail.com>
2025-08-03 14:47:47 -0300

    feat - criado e implementado metodo salvarNovaTransacao

--- src/main/java/com/ng/api/gestao/bancaria/repository/TransacaoRepository.java
+++ src/main/java/com/ng/api/gestao/bancaria/repository/TransacaoRepository.java
@@ -1,5 +1,8 @@
 package com.ng.api.gestao.bancaria.repository;
 
+import com.ng.api.gestao.bancaria.controller.dto.TransacaoDto;
+import com.ng.api.gestao.bancaria.model.Conta;
+import com.ng.api.gestao.bancaria.model.Transacao;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +11,13 @@ import org.springframework.stereotype.Repository;
 public class TransacaoRepository {
     private final ITransacaoJpaRepository transacaoRepository;
     private final IContaJpaRepository contaRepository;
+
+    public Transacao salvarNovaTransacao(TransacaoDto dto, Conta conta) {
+        Transacao transacao = new Transacao();
+        transacao.setFormaPagamento(dto.getForma_pagamento());
+        transacao.setValor(dto.getValor());
+        conta = contaRepository.save(conta);
+        transacao.setConta(conta);
+        return transacaoRepository.save(transacao);
+    }
 }
